7ec001c78ec3ef94500430d83533affcbd707311
hariprasanthzerobugz@gmail.com <hariprasanthzerobugz@gmail.com>
2022-08-25 10:27:33 +0530

    start banner

--- src/app/uitoux/uitoux.module.ts
+++ src/app/uitoux/uitoux.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { UitouxRoutingModule } from './uitoux-routing.module';
 import { UitouxComponent } from './uitoux.component';
+import { BannerComponent } from './banner/banner.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     UitouxComponent,
+    BannerComponent,
   ],
   imports: [
     CommonModule,
-    UitouxRoutingModule
+    UitouxRoutingModule,
+    NgbModule
   ]
 })
 export class UitouxModule { }
