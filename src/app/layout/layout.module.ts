97f4e2aab80f2147237728b2c29ea9c3c4c56100
hariprasanthzerobugz@gmail.com <hariprasanthzerobugz@gmail.com>
2022-08-25 10:04:37 +0530

    topbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopbarComponent
   ],
   imports: [
     CommonModule,
